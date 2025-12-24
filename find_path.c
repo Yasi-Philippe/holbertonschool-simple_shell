dfeb226741df0c213637ec0d1155b70e843fafe2
Yasi-Philippe <shyasi95@gmail.com>
2025-12-24 09:48:55 +0100

    fix: Added printf functions in order for debug process

--- find_path.c
+++ find_path.c
@@ -18,17 +18,26 @@ char **find_path(char *args[], char **env)
 	{
 		if (strncmp(env[i], "PATH", 4) == 0)
 		{
+			printf("%s\n", env[i]);
 			token = strtok(env[i], "=");
+			printf("%s\n", token);
 			token = strtok(NULL, "=");
+			printf("%s\n", token);
 			token_2 = strtok(token, ":");
+			printf("Test1\n%s", token_2);
 			while (token_2)
 			{
+				printf("Test1\n%s", token_2);
 				my_path = strcat(token_2, "/");
 				my_path_2 = strcat(my_path, args[0]);
+				printf("Test2\n");
 				if (access(my_path_2, X_OK) == 0)
 				{
+					printf("Test3\n");
 					free(args[0]);
+					printf("Test4\n");
 					args[0] = malloc(strlen(my_path_2) + 1);
+					printf("Test5\n");
 					strcpy(args[0], my_path_2);
 					return (args);
 				}
