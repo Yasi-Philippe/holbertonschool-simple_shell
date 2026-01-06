788d083833df7c0d0d669d15c16763b8ecc04679
Yasi-Philippe <shyasi95@gmail.com>
2026-01-06 11:06:08 +0100

    fix: Renamed functions to be more readable

--- main.h
+++ main.h
@@ -15,7 +15,7 @@ size_t strtok_arr_len(char *str);
 void free_args(char **args);
 char **find_path(char *args[], char **env);
 void exit_shell(char **args, char **commands);
-void simple_shell_2(char **commands, char **env);
+void ev_exec_cmd(char **commands, char **env);
 void print_env(char **env);
 
 #endif
